5d7d755786c53eabffef774f5b9e6023db41ca6f
Jeff Eaton <jeff@eaton.fyi>
2024-05-09 18:31:01 -0500

    First stub for the Property object

--- src/property.ts
+++ src/property.ts
@@ -1,3 +1,14 @@
+import is from '@sindresorhus/is';
+import { Propmaster } from './propmaster';
+
 export class Property {
-  
+  constructor(protected object: Propmaster, protected path: string) { }
+
+  any() {}
+  all() {}
+  none() {}
+
+  get value(): unknown {
+    return this.object.get(this.path);
+  }
 }
