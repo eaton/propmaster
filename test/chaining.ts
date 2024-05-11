096221f372945cb0fe2c7474a8f740b69db4bf28
Jeff Eaton <jeff@eaton.fyi>
2024-05-11 16:10:52 -0500

    Test the batching flag

--- test/chaining.ts
+++ test/chaining.ts
@@ -2,9 +2,18 @@ import test from 'ava';
 import { Propmaster } from '../src/propmaster.js'
 import * as values from './fixtures/values.js';
 
-test('chained string manipulation', t => {
+test('batched mutations', t => {
   const p = new Propmaster(values.arrays);
+  t.deepEqual(p.get('strings').value, ['first', 'second', 'third', 'fourth']);
 
   p.get('strings').join(',').changeCase('upper').split(',');
   t.deepEqual(p.get('strings').value, ['FIRST', 'SECOND', 'THIRD', 'FOURTH']);
+
+  p.options.batchMutations = true;
+  p.get('strings').join(',').changeCase('lower').split(',');
+  t.deepEqual(p.get('strings').value, ['FIRST', 'SECOND', 'THIRD', 'FOURTH']);
+
+  p.get('strings').join(',').changeCase('lower').split(',').set();
+  t.deepEqual(p.get('strings').value, ['first', 'second', 'third', 'fourth']);
 });
+
